d1b1855785473253a0f3379f56e9192adf65deb2
Evan Hazlett <ejhazlett@gmail.com>
2013-02-24 11:31:07 -0500

    removed static mapping for cloudfoundry

--- locksmith/urls.py
+++ locksmith/urls.py
@@ -26,11 +26,3 @@ urlpatterns = patterns('',
     url(r'^vault/', include('vault.urls')),
     url(r'', include('social_auth.urls')),
 )
-
-# to serve media in cloudfoundry
-if os.environ.has_key('VCAP_SERVICES') or settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATICFILES_DIRS[0],
-        }),
-    )
