143cef132cecf6958e9680705434ab35458ef96f
Evan Hazlett <ejhazlett@gmail.com>
2013-04-10 16:49:06 -0400

    raise 404 on invalid uuid

--- vault/views.py
+++ vault/views.py
@@ -15,7 +15,7 @@ from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
 from django.core.cache import cache
@@ -33,16 +33,22 @@ except ImportError:
 @login_required
 def index(request):
     ctx = {}
-    ctx['credential_groups'] = CredentialGroup.objects.filter(
-        owner=request.user).order_by('name')
+    try:
+        ctx['credential_groups'] = CredentialGroup.objects.filter(
+            owner=request.user).order_by('name')
+    except CredentialGroup.DoesNotExist:
+        raise Http404()
     return render_to_response('vault/index.html', ctx,
         context_instance=RequestContext(request))
 
 @login_required
 def group(request, uuid=None):
     ctx = {}
-    group = CredentialGroup.objects.get(uuid=uuid, owner=request.user)
-    ctx['group'] = group
+    try:
+        group = CredentialGroup.objects.get(uuid=uuid, owner=request.user)
+        ctx['group'] = group
+    except CredentialGroup.DoesNotExist:
+        raise Http404()
     return render_to_response('vault/group.html', ctx,
         context_instance=RequestContext(request))
 
