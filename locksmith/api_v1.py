c672d51ee19cdb11a365049ac448a84e35c7af67
Evan Hazlett <ejhazlett@gmail.com>
2013-04-12 01:23:37 -0400

    fixed bug with credential group display in api

--- locksmith/api_v1.py
+++ locksmith/api_v1.py
@@ -120,34 +120,35 @@ class CredentialGroupAuthorization(Authorization):
 
 class CredentialAuthorization(Authorization):
     def read_list(self, object_list, bundle):
-        return object_list.filter(owner=bundle.request.user)
+        return object_list.filter(groups__owner=bundle.request.user)
 
     def read_detail(self, object_list, bundle):
-        return object_list.filter(owner=bundle.request.user)
+        return object_list.filter(groups__owner=bundle.request.user)
 
     def create_list(self, object_list, bundle):
         return object_list
 
     def create_detail(self, object_list, bundle):
-        return bundle.obj.owner == bundle.request.user
+        return object_list.filter(groups__owner=bundle.request.user)
 
     def update_list(self, object_list, bundle):
         allowed = []
 
         # Since they may not all be saved, iterate over them.
         for obj in object_list:
-            if obj.owner == bundle.request.user:
-                allowed.append(obj)
+            for g in obj.groups:
+                if g.owner == bundle.request.user:
+                    allowed.append(obj)
         return allowed
 
     def update_detail(self, object_list, bundle):
-        return bundle.obj.owner == bundle.request.user
+        return object_list.filter(groups__owner=bundle.request.user)
 
     def delete_list(self, object_list, bundle):
-        return bundle.obj.owner == bundle.request.user
+        return object_list.filter(groups__owner=bundle.request.user)
 
     def delete_detail(self, object_list, bundle):
-        return bundle.obj.owner == bundle.request.user
+        return object_list.filter(groups__owner=bundle.request.user)
 
 class UserResource(ModelResource):
     class Meta:
